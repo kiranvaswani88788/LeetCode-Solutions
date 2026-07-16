068f88225326f166007c10728c07187bc5b2b30a
kiran vaswani <kiranvaswani88788@gmail.com>
2026-07-16 20:43:46 +0530

    Delete my-folder directory

--- my-folder/0136-single-number/solution.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int singleNumber(int[] nums) {
-   int XoR=0;
-   for(int i=0; i<nums.length; i++){
-    XoR=XoR^nums[i];
-     }
-     return XoR;
-}
-}
--- my-folder/0175-combine-two-tables/solution.sql
+++
@@ -1,2 +0,0 @@
-# Write your MySQL query statement below
-select p.firstName,p.lastname,a.city,a.state from person p left join address a on p.personId = a.personId;
