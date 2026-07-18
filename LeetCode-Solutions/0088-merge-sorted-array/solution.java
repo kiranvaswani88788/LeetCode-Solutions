650e25be4067d1da332ffaefc8b47aa4ab670af9
kiran vaswani <kiranvaswani88788@gmail.com>
2026-07-18 18:30:48 +0000

    [leetcode] Runtime - 0 ms (100.00%), Memory - 43.6 MB (89.29%)

---
+++ LeetCode-Solutions/0088-merge-sorted-array/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+      int index=m+n-1;
+      int pointer1=m-1;
+      int pointer2=n-1;
+
+      while(pointer1>=0 && pointer2>=0){
+        if(nums1[pointer1]>=nums2[pointer2]){
+            nums1[index--]=nums1[pointer1--];
+        }else{
+            nums1[index--]=nums2[pointer2--];
+        }
+      }
+      while(pointer2>=0){
+        nums1[index--]=nums2[pointer2--];
+      }
+      
+    }
+      
+}
